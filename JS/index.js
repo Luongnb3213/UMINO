f892b39a9baaf9cac156fb5bf7146377d85b9905
Luongnb3213 <luongnb3213@gmail.com>
2024-01-06 09:33:52 +0700

    update

--- JS/index.js
+++ JS/index.js
@@ -113,33 +113,179 @@ window.addEventListener("scroll", () => {});
 /* ======================================== 
    Trending slider
    ====================================== */
-const next_btn = document.querySelector(".next-btn");
-const back_btn = document.querySelector(".back-btn");
-const trending_list_product = document.querySelector(".trending_list_product");
-const product = document.querySelector(
-  ".trending_list_product .row .col:first-child"
-);
-next_btn.onclick = (e) => {
-  trending_list_product.style.scrollBehavior = "smooth";
-  trending_list_product.scrollLeft += product.clientWidth;
-  if (trending_list_product.scrollLeft % product.clientWidth !== 0) {
-    trending_list_product.scrollLeft =
-      Math.floor(trending_list_product.scrollLeft / product.clientWidth) *
-        product.clientWidth +
-      product.clientWidth;
-  }
-};
+const next_btns = document.querySelectorAll(".next-btn");
+const back_btns = document.querySelectorAll(".back-btn");
 
-back_btn.onclick = (e) => {
-  trending_list_product.style.scrollBehavior = "smooth";
-  trending_list_product.scrollLeft -= product.clientWidth;
-  if (trending_list_product.scrollLeft % product.clientWidth !== 0) {
-    trending_list_product.scrollLeft =
-      Math.floor(trending_list_product.scrollLeft / product.clientWidth) *
-      product.clientWidth;
-  }
-};
+Array.from(next_btns).forEach((next_btn) => {
+  next_btn.onclick = (e) => {
+    let parentElement = next_btn.parentElement;
+    let trending_list_product = parentElement.querySelector(
+      ".trending_list_product"
+    );
+    if (trending_list_product) {
+      let product = trending_list_product.querySelector(
+        ".row .col:first-child"
+      );
+      trending_list_product.style.scrollBehavior = "smooth";
+      trending_list_product.scrollLeft += product.clientWidth;
+      if (trending_list_product.scrollLeft % product.clientWidth !== 0) {
+        trending_list_product.scrollLeft =
+          Math.floor(trending_list_product.scrollLeft / product.clientWidth) *
+            product.clientWidth +
+          product.clientWidth;
+      }
+    }
+  };
+});
+Array.from(back_btns).forEach((back_btn) => {
+  back_btn.onclick = (e) => {
+    let parentElement = back_btn.parentElement;
+    let trending_list_product = parentElement.querySelector(
+      ".trending_list_product"
+    );
+    if (trending_list_product) {
+      let product = trending_list_product.querySelector(
+        ".row .col:first-child"
+      );
+      trending_list_product.style.scrollBehavior = "smooth";
+      trending_list_product.scrollLeft -= product.clientWidth;
+      if (trending_list_product.scrollLeft % product.clientWidth !== 0) {
+        trending_list_product.scrollLeft =
+          Math.floor(trending_list_product.scrollLeft / product.clientWidth) *
+          product.clientWidth;
+      }
+      console.log(trending_list_product.scrollLeft);
+    }
+  };
+});
+
+const next_btns_mobile = document.querySelectorAll(".mobile_button .next-btn");
+const back_btns_mobile = document.querySelectorAll(".mobile_button .back-btn");
+
+Array.from(next_btns_mobile).forEach((next_btn) => {
+  next_btn.onclick = (e) => {
+    let parentElement = next_btn.parentElement.parentElement;
+    let trending_list_product = parentElement.querySelector(
+      ".trending_list_product"
+    );
+    if (trending_list_product) {
+      let product = trending_list_product.querySelector(
+        ".row .col:first-child"
+      );
+      trending_list_product.style.scrollBehavior = "smooth";
+      trending_list_product.scrollLeft += product.clientWidth;
+      if (trending_list_product.scrollLeft % product.clientWidth !== 0) {
+        trending_list_product.scrollLeft =
+          Math.floor(trending_list_product.scrollLeft / product.clientWidth) *
+            product.clientWidth +
+          product.clientWidth;
+      }
+    }
+  };
+});
+Array.from(back_btns_mobile).forEach((back_btn) => {
+  back_btn.onclick = (e) => {
+    let parentElement = back_btn.parentElement.parentElement;
+    let trending_list_product = parentElement.querySelector(
+      ".trending_list_product"
+    );
+    if (trending_list_product) {
+      let product = trending_list_product.querySelector(
+        ".row .col:first-child"
+      );
+      trending_list_product.style.scrollBehavior = "smooth";
+      trending_list_product.scrollLeft -= product.clientWidth;
+      if (trending_list_product.scrollLeft % product.clientWidth !== 0) {
+        trending_list_product.scrollLeft =
+          Math.floor(trending_list_product.scrollLeft / product.clientWidth) *
+          product.clientWidth;
+      }
+      console.log(trending_list_product.scrollLeft);
+    }
+  };
+});
+
 /* ======================================== 
    END Trending slider
    ====================================== */
 
+/* ======================================== 
+  Hover image
+   ====================================== */
+const hover_images = document.querySelectorAll(".green");
+
+Array.from(hover_images).forEach((h) => {
+  h.onmouseover = function (e) {
+    let parentElement = e.target.closest(".col");
+    let image_change = parentElement.querySelector(
+      ".trending_product_image img:nth-child(1)"
+    );
+    let hover_button = parentElement.querySelectorAll(".green");
+    Array.from(hover_button).forEach((i) => {
+      i.classList.remove("active");
+    });
+    this.classList.add("active");
+    image_change.src = this.dataset.img;
+  };
+});
+
+/* ======================================== 
+   END Hover image
+   ====================================== */
+
+const footer_icon = document.querySelectorAll(".open-children-toogle");
+
+footer_icon.forEach((i) => {
+  i.onclick = function () {
+    let parentElement = i.parentElement.parentElement;
+    let footer_list = parentElement.querySelector(".footer_list");
+    // window.getComputedStyle để lấy giá trị của thuộc tính từ kiểu đã tính
+    //  (computed style) thay vì từ thuộc tính trực tiếp được thiết lập trong
+    //  style attribute.
+    // Ví dụ: ở trên nếu sử dụng  footer_list.style.maxHeight thì sẽ ra là = "" nên cần sử dụng  window.getComputedStyle để nó lấy luôn giá trị đã tính
+    let computedStyle = window.getComputedStyle(footer_list);
+
+    footer_list.style.maxHeight =
+      computedStyle.maxHeight === "0px" ? footer_list.scrollHeight + "px" : "0";
+    footer_list.classList.toggle("footer-list-padding");
+    this.classList.toggle("rotate");
+  };
+});
+
+var check = true;
+window.addEventListener("scroll", () => {
+  const windowHeight = document.documentElement.clientHeight;
+  const maxScrollHeight = document.documentElement.scrollHeight - windowHeight;
+  const back_top = document.querySelector(".back-top");
+
+  const scrollYValue = window.scrollY;
+  if (scrollYValue > 100) {
+    if (check) {
+      back_top.classList.add("back-top_show");
+      back_top.onclick = function () {
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth",
+        });
+      };
+      check = false;
+    }
+  } else {
+    if (!check) {
+      back_top.classList.remove("back-top_show");
+      check = true;
+    }
+  }
+
+  const percentage = ((scrollYValue / maxScrollHeight) * 100).toFixed(0);
+
+  const test = document.querySelector(".bls_back-top");
+
+  test.style.height = `${percentage}%`;
+
+  // let header_top_list = document.querySelector(".header-top-list");
+  // let computedStyle = window.getComputedStyle(header_top_list);
+  // header.classList.toggle("sticky", this.window.scrollY > header.clientHeight);
+  // // header_top_list.clientHeight
+  // header_top_list.classList.toggle("d-none", this.window.scrollY > header.clientHeight);
+});
